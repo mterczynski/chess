275b3ce56b9dca28c4e421770036c71665f5f123
Michał Terczyński <mterczynski271@gmail.com>
2021-10-09 21:59:59 +0200

    Add performance tests

---
+++ game-engine/e2e-tests/performance.e2e.test.ts
@@ -0,0 +1,18 @@
+import { playFoolsMate } from "../test-utils/playFoolsMate";
+
+describe('Performance', () => {
+    test('number of fool mate games played in a second should be at least 20', () => {
+        const start = Date.now();
+        let gamesPlayedInSecond = 0;
+
+        while (Date.now() - start < 1000) {
+            playFoolsMate();
+            gamesPlayedInSecond++;
+        }
+
+        console.log('Games played in:');
+        console.table([["second", gamesPlayedInSecond], ["minute", gamesPlayedInSecond * 60], ["hour", gamesPlayedInSecond * 60 * 60]]);
+
+        expect(gamesPlayedInSecond).toBeGreaterThan(20);
+    });
+});
