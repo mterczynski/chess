63a26bb4ee33036a4c6399e8d57f37aa7c6a5b74
Michał Terczyński <mterczynski1@gmail.com>
2022-10-30 23:21:53 +0100

    Handle draws and black wins

--- client/src/handle-game-end.tsx
+++ client/src/handle-game-end.tsx
@@ -1,6 +1,6 @@
-import { Game, GameState, Player } from "game-engine";
+import { GameState, Player } from "game-engine";
 
-export function handleGameEnd(game: Game, playerSide: Player) {
+export function handleGameEnd(gameState: GameState, playerSelection: Player) {
     const isDraw = [
         GameState.DRAW_BY_50_MOVE_RULE,
         GameState.DRAW_BY_75_MOVE_RULE,
@@ -8,17 +8,37 @@ export function handleGameEnd(game: Game, playerSide: Player) {
         GameState.DRAW_BY_INSUFFICIENT_MATERIAL,
         GameState.DRAW_BY_REPETITION,
         GameState.DRAW_BY_STALEMATE,
-    ].includes(game.getState());
+    ].includes(gameState);
 
-    if (playerSide === Player.WHITE) {
-        if (game.getState() === GameState.WHITE_WON) {
+    if (gameState === GameState.DRAW_BY_50_MOVE_RULE) {
+        return alert("Draw by 50 move rule");
+    } else if (gameState === GameState.DRAW_BY_75_MOVE_RULE) {
+        return alert("Draw by 75 move rule");
+    } else if (gameState === GameState.DRAW_BY_AGREEMENT) {
+        return alert("Draw by agreement");
+    } else if (gameState === GameState.DRAW_BY_INSUFFICIENT_MATERIAL) {
+        return alert("Draw by insufficient material");
+    } else if (gameState === GameState.DRAW_BY_REPETITION) {
+        return alert("Draw by repetition");
+    } else if (gameState === GameState.DRAW_BY_STALEMATE) {
+        return alert("Draw by stalemate");
+    }
+
+    if (playerSelection === Player.WHITE) {
+        if (gameState === GameState.WHITE_WON) {
             alert("You won");
-        } else if (game.getState() === GameState.BLACK_WON) {
+        } else if (gameState === GameState.BLACK_WON) {
             alert("You lost");
         } else if (isDraw) {
             alert("Game drawn"); // TODO - add more descriptive messages that mention draw reason
         }
     } else {
-        // TODO
+        if (gameState === GameState.BLACK_WON) {
+            alert("You won");
+        } else if (gameState === GameState.WHITE_WON) {
+            alert("You lost");
+        } else if (isDraw) {
+            alert("Game drawn"); // TODO - add more descriptive messages that mention draw reason
+        }
     }
 }
