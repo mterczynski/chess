65581641f24431661226d2a5b9b7d2707bcc45a7
Michał Terczyński <mterczynski1@gmail.com>
2025-05-22 16:26:00 +0200

    Add info bar component

--- client/src/board/Board.tsx
+++ client/src/board/Board.tsx
@@ -3,6 +3,23 @@ import styled from "styled-components";
 import { GameEngineContext } from "../GameEngineContext";
 import { File } from "./File";
 import { PromotionMenu } from "./PromotionMenu";
+import { Player } from "game-engine";
+
+const InfoBar = styled.div`
+    width: 100%;
+    max-width: 560px;
+    margin: 0 auto 10px auto;
+    padding: 0.7em 0;
+    text-align: center;
+    font-size: 1.2rem;
+    font-weight: 600;
+    color: #fff;
+    // background: #3d73ff;
+    // border-radius: 10px 10px 0 0;
+    box-shadow: 0 2px 8px rgba(0,0,0,0.08);
+    letter-spacing: 0.5px;
+    top: 30px;
+`;
 
 const BoardContainer = styled.div`
     display: flex;
@@ -27,13 +44,28 @@ const BoardContainer = styled.div`
 export const Board = () => {
     const gameEngineContext = useContext(GameEngineContext);
     const files = Object.values(gameEngineContext.board);
+    const currentPlayer = gameEngineContext.currentPlayer;
+
+    let infoText = null;
+    if (currentPlayer === undefined || currentPlayer === null) {
+        infoText = "Game Over";
+    } else if (currentPlayer === Player.WHITE) {
+        infoText = "White's turn";
+    } else if (currentPlayer === Player.BLACK) {
+        infoText = "Black's turn";
+    } else {
+        infoText = `${currentPlayer}'s turn`;
+    }
 
     return (
-        <BoardContainer>
-            {files.map((file, fileIndex) => (
-                <File file={file} fileIndex={fileIndex} key={fileIndex}></File>
-            ))}
-            <PromotionMenu />
-        </BoardContainer>
+        <>
+            <InfoBar>{infoText}</InfoBar>
+            <BoardContainer>
+                {files.map((file, fileIndex) => (
+                    <File file={file} fileIndex={fileIndex} key={fileIndex}></File>
+                ))}
+                <PromotionMenu />
+            </BoardContainer>
+        </>
     );
 };
