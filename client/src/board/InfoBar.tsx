81e73f04cb56b89ef4f42d98af69fc47fd142d06
Michał Terczyński <mterczynski1@gmail.com>
2025-05-22 16:32:46 +0200

    Format the code, remove !state check

--- client/src/board/InfoBar.tsx
+++ client/src/board/InfoBar.tsx
@@ -12,7 +12,7 @@ const InfoBarContainer = styled.div`
     font-size: 1.2rem;
     font-weight: 600;
     color: #fff;
-    box-shadow: 0 2px 8px rgba(0,0,0,0.08);
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.08);
     letter-spacing: 0.5px;
     top: 30px;
 `;
@@ -21,21 +21,30 @@ export const InfoBar = () => {
     const { currentPlayer, state } = useContext(GameEngineContext);
 
     let infoText = null;
-    if (!state || state === GameState.IN_PROGRESS || state === GameState.UNSTARTED) {
-        if (currentPlayer === Player.WHITE) infoText = "White's turn";
-        else if (currentPlayer === Player.BLACK) infoText = "Black's turn";
-        else infoText = "";
-    } else if (state === GameState.WHITE_WON) infoText = "White wins!";
-    else if (state === GameState.BLACK_WON) infoText = "Black wins!";
-    else if (
+    if (state === GameState.IN_PROGRESS || state === GameState.UNSTARTED) {
+        if (currentPlayer === Player.WHITE) {
+            infoText = "White's turn";
+        } else if (currentPlayer === Player.BLACK) {
+            infoText = "Black's turn";
+        } else {
+            infoText = "";
+        }
+    } else if (state === GameState.WHITE_WON) {
+        infoText = "White wins!";
+    } else if (state === GameState.BLACK_WON) {
+        infoText = "Black wins!";
+    } else if (
         state === GameState.DRAW_BY_STALEMATE ||
         state === GameState.DRAW_BY_INSUFFICIENT_MATERIAL ||
         state === GameState.DRAW_BY_REPETITION ||
         state === GameState.DRAW_BY_50_MOVE_RULE ||
         state === GameState.DRAW_BY_75_MOVE_RULE ||
         state === GameState.DRAW_BY_AGREEMENT
-    ) infoText = "Draw!";
-    else infoText = "Game Over";
+    ) {
+        infoText = "Draw!";
+    } else {
+        infoText = "Game Over";
+    }
 
     return <InfoBarContainer>{infoText}</InfoBarContainer>;
 };
