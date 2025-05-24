b5ac26575f8eac6515766acd52791665868000f2
Michał Terczyński <mterczynski1@gmail.com>
2025-05-24 17:23:29 +0200

    Add more openings

--- game-engine/src/openings/openings.ts
+++ game-engine/src/openings/openings.ts
@@ -145,4 +145,83 @@ export const openings: Opening[] = [
             { from: { file: ChessFile.F, rank: 2 }, to: { file: ChessFile.F, rank: 3 } },
         ],
     },
+    {
+        name: "Scandinavian Defense",
+        moves: [
+            { from: { file: ChessFile.E, rank: 2 }, to: { file: ChessFile.E, rank: 4 } },
+            { from: { file: ChessFile.D, rank: 7 }, to: { file: ChessFile.D, rank: 5 } },
+        ],
+    },
+    {
+        name: "Polish Opening",
+        moves: [
+            { from: { file: ChessFile.B, rank: 2 }, to: { file: ChessFile.B, rank: 4 } },
+        ],
+    },
+    {
+        name: "Four Knights Game",
+        moves: [
+            { from: { file: ChessFile.E, rank: 2 }, to: { file: ChessFile.E, rank: 4 } },
+            { from: { file: ChessFile.E, rank: 7 }, to: { file: ChessFile.E, rank: 5 } },
+            { from: { file: ChessFile.G, rank: 1 }, to: { file: ChessFile.F, rank: 3 } },
+            { from: { file: ChessFile.G, rank: 8 }, to: { file: ChessFile.F, rank: 6 } },
+            { from: { file: ChessFile.B, rank: 1 }, to: { file: ChessFile.C, rank: 3 } },
+            { from: { file: ChessFile.B, rank: 8 }, to: { file: ChessFile.C, rank: 6 } },
+        ],
+    },
+    {
+        name: "Pirc Defense",
+        moves: [
+            { from: { file: ChessFile.E, rank: 2 }, to: { file: ChessFile.E, rank: 4 } },
+            { from: { file: ChessFile.D, rank: 7 }, to: { file: ChessFile.D, rank: 6 } },
+            { from: { file: ChessFile.D, rank: 2 }, to: { file: ChessFile.D, rank: 4 } },
+            { from: { file: ChessFile.G, rank: 8 }, to: { file: ChessFile.F, rank: 6 } },
+        ],
+    },
+    {
+        name: "Alekhine's Defense",
+        moves: [
+            { from: { file: ChessFile.E, rank: 2 }, to: { file: ChessFile.E, rank: 4 } },
+            { from: { file: ChessFile.G, rank: 8 }, to: { file: ChessFile.F, rank: 6 } },
+        ],
+    },
+    {
+        name: "Nimzo-Indian Defense",
+        moves: [
+            { from: { file: ChessFile.D, rank: 2 }, to: { file: ChessFile.D, rank: 4 } },
+            { from: { file: ChessFile.G, rank: 8 }, to: { file: ChessFile.F, rank: 6 } },
+            { from: { file: ChessFile.C, rank: 2 }, to: { file: ChessFile.C, rank: 4 } },
+            { from: { file: ChessFile.E, rank: 7 }, to: { file: ChessFile.E, rank: 6 } },
+            { from: { file: ChessFile.B, rank: 1 }, to: { file: ChessFile.C, rank: 3 } },
+            { from: { file: ChessFile.B, rank: 8 }, to: { file: ChessFile.B, rank: 6 } },
+            { from: { file: ChessFile.C, rank: 1 }, to: { file: ChessFile.G, rank: 5 } },
+        ],
+    },
+    {
+        name: "Dutch Defense",
+        moves: [
+            { from: { file: ChessFile.D, rank: 2 }, to: { file: ChessFile.D, rank: 4 } },
+            { from: { file: ChessFile.F, rank: 7 }, to: { file: ChessFile.F, rank: 5 } },
+        ],
+    },
+    {
+        name: "London System",
+        moves: [
+            { from: { file: ChessFile.D, rank: 2 }, to: { file: ChessFile.D, rank: 4 } },
+            { from: { file: ChessFile.D, rank: 7 }, to: { file: ChessFile.D, rank: 5 } },
+            { from: { file: ChessFile.B, rank: 1 }, to: { file: ChessFile.F, rank: 4 } },
+        ],
+    },
+    {
+        name: "Reti Opening",
+        moves: [
+            { from: { file: ChessFile.G, rank: 1 }, to: { file: ChessFile.F, rank: 3 } },
+        ],
+    },
+    {
+        name: "Grob Attack",
+        moves: [
+            { from: { file: ChessFile.G, rank: 2 }, to: { file: ChessFile.G, rank: 4 } },
+        ],
+    },
 ];
