df1fc2dd360d5c840ea1c5b66ea31fe88462d5d7
Michał Terczyński <mterczynski1@gmail.com>
2025-05-24 21:15:30 +0200

    Add nestjs dev tools (for debugging)

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        snapshot: true, // enables debugging https://docs.nestjs.com/devtools/overview
+    });
     await app.listen(process.env.PORT ?? 3000);
     console.log(`Application running at ${await app.getUrl()}`);
 }
