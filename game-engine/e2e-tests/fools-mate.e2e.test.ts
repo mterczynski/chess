803ecda5563c09fb1f1c0edd9828113bc0cbe81b
Michał Terczyński <mterczynski1@gmail.com>
2025-05-24 14:49:55 +0200

    Improve style of e2e test comments

--- game-engine/e2e-tests/fools-mate.e2e.test.ts
+++ game-engine/e2e-tests/fools-mate.e2e.test.ts
@@ -1,11 +1,10 @@
+import { GameState } from "../src/GameState";
+import { playFoolsMate } from "../test-utils/playFoolsMate";
+
 /**
  * Win for black in 2 moves
  * https://en.wikipedia.org/wiki/Fool%27s_mate#:~:text=In%20chess%2C%20Fool's%20Mate%2C%20also,second%20move%20with%20the%20queen.
  */
-
-import { GameState } from "../src/GameState";
-import { playFoolsMate } from "../test-utils/playFoolsMate";
-
 describe(`Fools's mate`, () => {
     test('play all the moves, check that black has won', () => {
         const game = playFoolsMate();
