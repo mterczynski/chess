e2488d8f70310e4f8318cf78fc39036e43616cd2
Michał Terczyński <mterczynski1@gmail.com>
2025-05-24 19:17:26 +0200

    Add basic lobby controller

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LobbyController } from './lobby/lobby.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, LobbyController],
   providers: [AppService],
 })
 export class AppModule {}
