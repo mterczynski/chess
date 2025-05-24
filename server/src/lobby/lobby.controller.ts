0f348a6165d5b0ca46ab4cadc9d9feadcba753de
Michał Terczyński <mterczynski1@gmail.com>
2025-05-24 19:18:51 +0200

    Add createLobby endpoint

--- server/src/lobby/lobby.controller.ts
+++ server/src/lobby/lobby.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+
+const lobbies: { name: string; password: string }[] = [];
 
 @Controller('lobby')
-export class LobbyController {}
+export class LobbyController {
+    @Post()
+    createLobby(@Body() body: { name: string; password: string }) {
+        if (
+            typeof body.name !== 'string' ||
+            typeof body.password !== 'string'
+        ) {
+            return {
+                success: false,
+                message: 'Name and password must be strings.',
+            };
+        }
+        lobbies.push({ name: body.name, password: body.password });
+        return { success: true };
+    }
+}
