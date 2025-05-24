e539bee8faf91c21d0d041702eff07d0f92515c5
Michał Terczyński <mterczynski1@gmail.com>
2025-05-24 04:25:20 +0200

    Add popup

--- client/src/GameScreenSelector.tsx
+++ client/src/GameScreenSelector.tsx
@@ -1,14 +1,20 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Board } from "./board/Board";
 import { GameClientContext } from "./GameClientContext";
 import { PlayerSelectionScreen } from "./PlayerSelectionScreen";
+import { ModeSelectionScreen, GameMode } from "./ModeSelectionScreen";
 
 export const GameScreenSelector = () => {
     const gameClientContext = useContext(GameClientContext);
+    const [mode, setMode] = useState<GameMode | null>(null);
 
-    return gameClientContext.playerSelection === null ? (
-        <PlayerSelectionScreen />
-    ) : (
-        <Board />
-    );
+    if (!mode) {
+        return <ModeSelectionScreen onSelect={setMode} />;
+    }
+
+    if (gameClientContext.playerSelection === null) {
+        return <PlayerSelectionScreen mode={mode} />;
+    }
+
+    return <Board />;
 };
