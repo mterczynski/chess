9cea2c83c3ef068d8d314e4c87604b2f0f2d5463
Michał Terczyński <mterczynski1@gmail.com>
2025-05-25 23:53:33 +0200

    Remove outdated comment

--- client/src/GameMode.ts
+++ client/src/GameMode.ts
@@ -1,5 +1,5 @@
 export enum GameMode {
     VS_BOT = "vs_bot",
     VS_PLAYER_OFFLINE = "vs_player_offline",
-    VS_PLAYER_ONLINE = "vs_player_online", // unused for now
+    VS_PLAYER_ONLINE = "vs_player_online",
 }
