93532b7dc6aa47894212d9b3dedeee64c3081bcd
Michał Terczyński <mterczynski1@gmail.com>
2025-05-25 15:42:03 +0200

    Enable cors for 5173 and 5174 ports

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         snapshot: true, // enables debugging https://docs.nestjs.com/devtools/overview
     });
+
+    app.enableCors({
+        origin: ["http://localhost:5173", "http://localhost:5174"],
+    });
+
     await app.listen(process.env.PORT ?? 3000);
     console.log(`Application running at ${await app.getUrl()}`);
 }
