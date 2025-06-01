1c411dbec1c30c1a7944726b0e93b6d8270fdf2e
Michał Terczyński <mterczynski1@gmail.com>
2025-06-01 20:34:44 +0200

    Add settings icon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,24 @@
+import styled from "styled-components";
 import "./App.css";
 import { GameClientContextProvider } from "./contexts/GameClientContext";
 import { GameEngineContextProvider } from "./contexts/GameEngineContext";
 import { GameScreenSelector } from "./GameScreenSelector";
+
+const SettingsIcon = styled.div`
+    position: absolute;
+    top: 10px;
+    right: 10px;
+`;
 
 function App() {
     return (
         <div className="App">
             <GameEngineContextProvider>
                 <GameClientContextProvider>
-                    <GameScreenSelector />
+                    <>
+                        <SettingsIcon>⚙️</SettingsIcon>
+                        <GameScreenSelector />
+                    </>
                 </GameClientContextProvider>
             </GameEngineContextProvider>
         </div>
